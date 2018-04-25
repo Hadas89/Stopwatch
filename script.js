6d3541e1ecd3cb9acc5b26138ee773a9f198ea42
Hadas Salomon <Hadas89@gmail.com>
2018-04-25 15:51:21 +0300

    another method - getting there, almost done

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
 console.log("check")
 
+		var msDisplay = document.querySelector('.milliseconds')
+		var sDisplay = document.querySelector('.seconds')
+		var mDisplay = document.querySelector('.minutes')
+		ms = 0, s = 0 , m = 0
+
+var clockUpdate  = {
+
+	update: function () {
+		Interval = setInterval(function() {
+			ms++;
+			msDisplay.innerHTML = ms;
+			
+
+			if (ms >10) {
+				ms = 0;
+
+					s++;
+					sDisplay.innerHTML = s + ":"
+					
+
+					if (s > 59) {
+						s = 0;
+
+						m++;
+						mDisplay.innerHTML = m + ":"
+
+					}
+			}
+}, 100)
+		},
+
+	startResumeCount : function () {
+	
+	clockUpdate.update()
+
+},
+
+	pauseCount : function () {
+	
+	  clearInterval(Interval)
+	 
+},
+
+	resetCount : function () {
+	document.querySelector('.milliseconds').innerHTML ="000"
+	document.querySelector('.seconds').innerHTML = "00 :"
+	document.querySelector('.minutes').innerHTML = "00 :"
+	},
+	
+}
+
+
+
+startresumecount.addEventListener('click', clockUpdate.startResumeCount)
+pausecount.addEventListener('click', clockUpdate.pauseCount)
+resetcount.addEventListener('click', clockUpdate.resetCount)
+
+
+//*previous tries*
+
 
 // var startCount = function () {
  
@@ -81,85 +141,56 @@ console.log("check")
 
 
 // },60*1000)
-var clockUpdate = {
+// var clockUpdate = {
 	
 
-	update: function () {
+// 	update: function () {
 		
-	thenMS = new Date().getMilliseconds()
-	thenS = new Date().getSeconds()
-	thenM = new Date().getMinutes()
-	this.milliSeconds()
-	this.Seconds()
-	this.Minutes()
-	},
+// 	thenMS = new Date().getMilliseconds()
+// 	thenS = new Date().getSeconds()
+// 	thenM = new Date().getMinutes()
+// 	this.milliSeconds()
+// 	this.Seconds()
+// 	this.Minutes()
+// 	},
 
 
-	milliSeconds: function() {
-		msInterval = setInterval(function() {
-		ms = new Date().getMilliseconds()
+// 	milliSeconds: function() {
+// 		msInterval = setInterval(function() {
+// 		ms = new Date().getMilliseconds()
 	
-		document.querySelector('.milliseconds').innerHTML = (this.thenMS + ms)
-		}, 1)
-},
+// 		document.querySelector('.milliseconds').innerHTML = (this.thenMS + ms)
+// 		}, 1)
+// },
 
 
-	Seconds: function() {
-		sInterval = setInterval(function() {
-		s = new Date().getSeconds()
+// 	Seconds: function() {
+// 		sInterval = setInterval(function() {
+// 		s = new Date().getSeconds()
 
-		document.querySelector('.seconds').innerHTML = (s - this.thenS) + " : "
-		}, 1)
-},
+// 		document.querySelector('.seconds').innerHTML = (s - this.thenS) + " : "
+// 		}, 1)
+// },
 
-	Minutes: function() {
-		mInterval = setInterval(function() {
-		m = new Date().getMinutes()
+// 	Minutes: function() {
+// 		mInterval = setInterval(function() {
+// 		m = new Date().getMinutes()
 		
-		document.querySelector('.minutes').innerHTML = m - this.thenM + " :" 
-		}, 1)
-},
-
-	
-}
+// 		document.querySelector('.minutes').innerHTML = m - this.thenM + " :" 
+// 		}, 1)
+// },
 
 
 
 
 
+//evolution of progress!
+//12.4 this task is way to complicated in WDI, try somewhere else (good vids around including of 30dayschallenge 
+//https://courses.wesbos.com/account/access/5a79a8df4ea3f06ee90376bf or https://www.youtube.com/results?search_query=stopwatch+javascript
+//also before these videos try to create the stopwatch to display without the oo: (i will be for seconds x for minutes?)
 
+//23/4 continue this app (alone) with the help of https://www.youtube.com/watch?v=jRhB1IG7uAw - oop so too difficult. try this https://www.youtube.com/watch?v=fF-vtP3sjPcas you progress
+//check why setinterval only works when function is fully written inside it
+//https://forum.freecodecamp.org/search?q=pomodoro%20difficult read - I imply from here that the stopwatch project is advanced FE
 
-// }
-
-
-var startCount = function () {
-
-	
-	clockUpdate.update()
-}
-
-
-var stopCount = function () {
-	
- clearInterval(msInterval)
- clearInterval(sInterval)
- clearInterval(mInterval)
-
-}
-
-var resetCount = function () {
-document.querySelector('.milliseconds').innerHTML =" 00"
-document.querySelector('.seconds').innerHTML = "00 :"
-document.querySelector('.minutes').innerHTML = "00 :"
-}
-
-
-
-
-startcount.addEventListener('click', startCount)
-stopcount.addEventListener('click', stopCount)
-resetcount.addEventListener('click', resetCount)
-
-
-
-
+//25/4 try to tackle resuming from the last point on time, continue googling/wdi video lesson (currentTime = 0)
